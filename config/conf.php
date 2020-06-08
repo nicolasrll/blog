0bb8ff15672ed8df2c42b4cd9356b19ed260debb
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-06-08 11:57:15 +0200

    feat #2 : .gitignored is now working on public folder and .composer.json modified on public folder
    issue #2

--- config/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    // To be modified by your own configuration
-    define('DSN', 'mysql:host=localhost');
-    define('DBNAME', 'blog');
-    define('DBUSER', 'root');
-    define('DBPASSWORD', '');
