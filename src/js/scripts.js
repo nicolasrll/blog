d9d0417e98b5a4fe2fe0b1490e753c21d4c261d9
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-09-15 23:34:05 +0200

    style #14 (header): header navbar-shrink and submenu
    Navar shrink fixed
    dropdown item active color in submenu
    
    style #14

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,10 +3,10 @@
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
+        if ($(".mainNav").offset().top > 100) {
+            $(".mainNav").addClass("navbar-shrink");
         } else {
-            $("#mainNav").removeClass("navbar-shrink");
+            $(".mainNav").removeClass("navbar-shrink");
         }
     };
     // Collapse now if page is not at top
