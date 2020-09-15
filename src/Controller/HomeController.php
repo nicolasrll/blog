4e76a4ed765cc1811f58ed2a16396ce24f1602da
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-09-15 12:00:02 +0200

    style #14 (home, header): few corrections made
    Portfolio replace project in header and anchor
    Fermer replace close in modal fade
    Date formatted to display only day month and years on home page project
    Width problem on mobile resolved
    Project setId with object method instead of specific variable
    
    style #14

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,21 +3,18 @@
 namespace App\Controller;
 
 use Core\DefaultControllerAbstract;
-use Core\Request;
 use App\Repository\ProjectManager;
 use App\Entity\Project;
-
 
 class HomeController extends DefaultControllerAbstract
 {
     public function indexAction(): void
     {
         $projects = (new ProjectManager())->find();
-        $i = 1;
+
         foreach ($projects as $value) {
             $formattedEntity = $this->formatTheEntity($value, ['chapo', 'content']);
-            $formattedProject[] = (new Project())->hydrate($formattedEntity)->setId($i);
-            $i++;
+            $formattedProject[] = (new Project())->hydrate($formattedEntity)->setId($value->getId());
         }
 
         $this->renderView(
