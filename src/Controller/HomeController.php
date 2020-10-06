7a35e4996483d4daa2758b42c37245417423b493
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-10-06 21:32:59 +0200

    feat #15 (HomeController, Message): contact form created on home page
    feat #15

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Core\DefaultControllerAbstract;
 use App\Repository\ProjectManager;
+use App\Repository\MessageManager;
 use App\Entity\Project;
+use App\Entity\Message;
 
 class HomeController extends DefaultControllerAbstract
 {
@@ -17,11 +19,65 @@ class HomeController extends DefaultControllerAbstract
             $formattedProject[] = (new Project())->hydrate($formattedEntity)->setId($value->getId());
         }
 
+        //$flashMessage = '';
+        //$classValue = 'text-danger';
+        $_SESSION['classValue'] = 'text-danger';
+
+        if ($this->isSubmited('contact')) {
+            $formValues = $this->getFormValues('contact');
+
+            if (
+                $this->tokenCSRFIsValid($formValues['tokenContactForm'])
+                && $this->formFieldsIsNotEmpty($formValues)
+            ) {
+                $messageEntity = (new Message())->hydrate($formValues)->setSentOn(date('Y-m-d H:i:s'));
+                $messageRepository = (new MessageManager())->insert($messageEntity);
+
+                if ("0" !== $messageRepository) {
+                    $emailSent = $this->sentEmail($messageEntity);
+                    //$_SESSION['flashMessage'] = $emailSent ? 'Votre message a bien été envoyé' : 'Une erreur est survenue, votre message n\'a pas pu être envoyer';
+                    //$_SESSION['classValue'] = $emailSent ? 'text-success' : 'text-danger';
+                }
+
+
+                $_SESSION['flashMessage'] = "0" !== $messageRepository && $emailSent ? 'Votre message a bien été envoyé' : 'Une erreur est survenue, votre message n\'a pas pu être envoyer';
+                $classValue = 'Votre message a bien été envoyé' === $_SESSION['flashMessage'] ? 'text-success' : 'text-danger';
+            }
+        }
+
+        $this->generateTokenCSRF();
         $this->renderView(
             'home.html.twig',
             [
-                'projects' => $formattedProject
+                'projects' => $formattedProject,
+                'flashMessage' => $_SESSION['flashMessage'] ?? '',
+                'classValue' => $_SESSION['classValue'] ?? ''
             ]
+        );
+    }
+
+    private function sentEmail(Message $messageEntity): bool
+    {
+        $messageFormattedEntity = $this->formatTheEntity($messageEntity, ['message']);
+        $message = '
+            <html>
+                <head>
+                    <title>Nouveau message</title>
+                </head>
+                <body>
+                    <p>Envoyé par ' . $messageEntity->getFullname(). '</p>
+                    <p>' . $messageFormattedEntity['message'] . '</p>
+                </body>
+            </html>
+           ';
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf-8';
+
+        return mail(
+            'nicolasrellier@yahoo.fr',
+            'Nouveau message envoyé depuis ton site',
+            $message,
+            implode("\r\n", $headers)
         );
     }
 }
